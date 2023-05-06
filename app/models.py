6ea8715299530d6f9bb556fefc8c3be4bb7f5ac1
shawazi <shawazisonfire@gmail.com>
2023-05-06 17:04:42 -0400

    updated with rafe's model intntro

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    number = models.IntegerField()
+    email = models.EmailField()
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    
